457ed51475d4fbbe0719593d55a3091f95afd278
松浦 雅人 <masato@masato-matsuuras-macbook-air.local>
2017-03-06 23:09:26 +0900

    Initial Commit

---
+++ placeBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  placeBook
+//
+//  Created by 松浦 雅人 on 2017/03/06.
+//  Copyright © 2017年 松浦 雅人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
